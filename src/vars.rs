b3e5fed59fe3693f1a3290098a7cff451e872bc7
Juan Carlos Carmona <juan@Juans-MacBook-Pro.local>
2020-04-11 18:01:45 -0500

    Types and Vars

--- src/vars.rs
+++ src/vars.rs
@@ -4,6 +4,18 @@
 
 pub fn run() {
     let name = "Brad";
+    let mut age = 37;
+    println!("My name is {} and I am {}", name, age);
 
-    println!("My name is {}", name);
+    age = 38;
+    println!("My name is {} and I am {}", name, age);
+
+    // Define constant
+    const ID: i32 = 001;
+
+    println!("ID: {}", ID);
+
+    // Assign multiple variables
+    let (my_name, my_age) = ("Brad", 37);
+    println!("{} is {}", my_name, my_age);
 }
